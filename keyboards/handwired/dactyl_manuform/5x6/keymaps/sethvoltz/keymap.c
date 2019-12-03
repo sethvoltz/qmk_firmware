0eba14ee60f063764f560b80fab7cb3c35c2cd67
Seth Voltz <svoltz@box.com>
2019-12-03 15:50:49 -0800

    Update sethvoltz keymap with new backspace placement

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/sethvoltz/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/sethvoltz/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                                 KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_EQUAL,
     KC_LSFT,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                                 KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                                 KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLASH,
-                        _______,  _______,                                                                      KC_LBRC,  KC_RBRC,
+                        _______,  _______,                                                                      _______,  _______,
                                                       RAISE,    KC_SPC,           KC_ENT,   LOWER,
                                                       KC_LALT,  KC_LGUI,          KC_RGUI,  KC_RALT,
                                                       KC_LCTL,  KC_TAB,           KC_TAB,   KC_RCTL
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  KC_HOME,  KC_PGDN,  KC_END ,  KC_LPRN,                              KC_RPRN,  KC_P4,    KC_P5,    KC_P6,    _______,  _______,
     _______,  _______,  _______,  _______,  _______,  KC_MINS,                              KC_UNDS,  KC_P1,    KC_P2,    KC_P3,    _______,  _______,
                         _______,  _______,                                                                      _______,  KC_P0,
-                                                      _______,  _______,          KC_DEL,   _______,
+                                                      _______,  KC_BSPC,          KC_DEL,   _______,
                                                       _______,  _______,          _______,  _______,
                                                       _______,  _______,          _______,  _______
 
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_LPRN,                              KC_RPRN,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,  KC_VOLD,
     _______,  _______,  _______,  _______,  _______,  KC_MINS,                              KC_UNDS,  _______,  _______,  _______,  _______,  KC_MUTE,
                         _______,  _______,                                                                      _______,  _______,
-                                                      _______,  _______,          KC_BSPC,  _______,
+                                                      _______,  KC_BSPC,          KC_DEL,   _______,
                                                       _______,  _______,          _______,  _______,
                                                       _______,  _______,          _______,  _______
   ),
